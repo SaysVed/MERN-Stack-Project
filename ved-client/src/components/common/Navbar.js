05c987aeddcb924bd2372bff304895fd28c018d2
Ved <contact.vedant18@gmail.com>
2023-09-16 10:47:19 +0530

    server bugs resolved

--- ved-client/src/components/common/Navbar.js
+++ ved-client/src/components/common/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
         <div className="bg-black top-0 w-full h-16 flex justify-evenly items-center text-white shadow-sm font-mono fixed" role="navigation">        
             <div className="pl-8 ">
                 <ul className="flex ">
-                    <li className="p-4"> <a href="#" className="text-sky-400 ">Home</a></li>
+                    <li className="p-4"> <a href="http://localhost:3000/" className="text-sky-400 ">Home</a></li>
                     <li className="p-4"><a href="#">About</a></li>
                     <li className="p-4"><a href="#">Shop</a></li>
                     <li className="p-4"><a href="/contact">Contact Us</a></li>
